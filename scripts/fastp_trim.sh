a9f5811f51a10b4e77327f7f2a4a29382614ac02
Kariuki Kimani <KWTRP\kkimani@keklf-cls05.kwtrp.org>
2024-08-26 14:42:50 +0300

    added reports after trimming

---
+++ scripts/fastp_trim.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# The path to the files to be trimmed 
+input_dir=/home/KWTRP/kkimani/Home/alzepi/output
+cd $input_dir
+
+#makking a  directory for the results  of fastp
+mkdir ./fastp
+
+# a loop to iterate through the sequences in the input directory 
+for  sra_seq in *_1.fastq.gz; do
+	name=$(basename ${sra_seq} _1.fastq.gz) #remove the end of the file
+	echo ${name}
+#running the fastp for the file
+	fastp -i ${name}_1.fastq.gz -I ${name}_2.fastq.gz -o ./fastp/${name}_1.trim.fastq.gz -O ./fastp/${name}_2.trim.fastq.gz --failed_out ./fastp/${name}_failed --detect_adapter_for_pe --trim_poly_x --trim_poly_g --report_title -h ${name}_report.html
+done
+echo 'all done'
